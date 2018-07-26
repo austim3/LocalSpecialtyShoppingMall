1ccb95eff4ac97a02ff3c0342e274194381b29b3
Your Name <you@example.com>
2018-07-26 09:47:49 +0800

    aa

--- src/DataManager/AddressManager.js
+++ src/DataManager/AddressManager.js
@@ -8,6 +8,8 @@ class AddressManager{
     async AddAddress(CustomerID,Addressee,Phone,City,Province,District,Detailedaddress){
         try {
             const fromData=new FormData();
+            const Uid=localStorage.Uid;
+            const token =localStorage.token;
             fromData.append('CustomerID',CustomerID);
             fromData.append('Addressee',Addressee);
             fromData.append('Phone',Phone);
@@ -30,18 +32,35 @@ class AddressManager{
         }
 
     }
-    async UpdateAddress(CustomerID,Addressee,Phone,City,Province,District,Detailedaddress){
+    async UpdateAddress(userInfo){
         try {
+            const token =localStorage.token;
+            const Uid=localStorage.Uid;
             const fromData=new FormData();
-            fromData.append('CustomerID',CustomerID);
-            fromData.append('Addressee',Addressee);
-            fromData.append('Phone',Phone);
-            fromData.append('City',City);
-            fromData.append('Province',Province);
-            fromData.append('District',District);
-            fromData.append('Detailedaddress',Detailedaddress);
+            fromData.append('token',token);
+            if(userInfo.CustomerID){
+                fromData.append('CustomerID',userInfo.CustomerID);
+            }
+            if(userInfo.Addressee){
+                fromData.append('Addressee',userInfo.Addressee);
+            }
+            if(userInfo.Phone){
+                fromData.append('Phone',userInfo.Phone);
+            }
+            if(userInfo.City){
+                fromData.append('City',userInfo.City);
+            }
+            if (userInfo.Province) {
+                fromData.append('Province',userInfo.Province);
+            }
+            if (userInfo.District) {
+                fromData.append('District',userInfo.District);
+            }
+            if (userInfo.Detailedaddress) {
+                fromData.append('Detailedaddress',userInfo.Detailedaddress);
+            }
 
-            const res=await fetch(AddressUrl,{
+            const res=await fetch(Get,{
                 method:'POST',
                 body:fromData,
             })
@@ -57,6 +76,14 @@ class AddressManager{
     }
     async deleteAddress(ID){
         try{
+            const token=localStorage.token;
+            const Uid=localStorage.Uid;
+            const res=await fetch(deleteAddressUrl,{
+                token,
+                Uid,
+                ID
+            })
+            return res.data;
 
         }catch(error){
             return{
@@ -65,6 +92,22 @@ class AddressManager{
             }
         }
 
+    }
+    async getAddress(){
+        try {
+            const token =localStorage.token;
+            const Uid=localStorage.Uid;
+            const res=await fetch(Get,{
+                token,
+                Uid
+            })
+            return res.data;
+        } catch (error) {
+            return {
+                success:false,
+                errorMessage:'网络错误'
+            }
+        }
     }
 }
 export default new FriendManager();
